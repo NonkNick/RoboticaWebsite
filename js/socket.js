e6e2a91e40a33456568c92349618350d904d896f
NonkNick <honossillie@gmail.com>
2022-05-18 11:00:31 +0200

    aspect ratio fotos

--- js/socket.js
+++ js/socket.js
@@ -1,1 +1,12 @@
-const socket = new WebSocket('ws://localhost:5005');
+window.addEventListener("DOMContentLoaded", () => {
+    const messages = document.createElement("ul");
+    document.body.appendChild(messages);
+
+    const websocket = new WebSocket("ws://localhost:5678/");
+    websocket.onmessage = ({ data }) => {
+        const message = document.createElement("li");
+        const content = document.createTextNode(data);
+        message.appendChild(content);
+        messages.appendChild(message);
+    };
+});
